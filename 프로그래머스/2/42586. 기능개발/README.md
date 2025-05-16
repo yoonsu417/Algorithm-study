2099c224c1024b949dbad1b4d798095820682fab
Kim YoonSu <121972417+yoonsu417@users.noreply.github.com>
2025-05-16 12:53:48 +0900

    [level 2] Title: 기능개발, Time: 0.05 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 83.3 MB, 시간: 0.41 ms
+메모리: 9.19 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 05월 16일 12:46:47
+2025년 05월 16일 12:53:47
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,3 +1,28 @@
-def solution(progresses, speeds):
+from collections import deque
+import math
+
+def solution(progresses, speeds):    
     answer = []
+    
+    speedsQueue = deque(speeds)
+    progressQueue = deque(progresses)
+    days = 0
+    
+    while progressQueue:
+        currentProgress = progressQueue[0] + speedsQueue[0] * days
+        
+        if currentProgress < 100:
+            remainDays = 100 - currentProgress
+            needDays = math.ceil(remainDays / speedsQueue[0])
+            days += needDays
+
+        count = 0
+        while progressQueue and progressQueue[0] + speedsQueue[0] * days >= 100:
+            progressQueue.popleft()
+            speedsQueue.popleft()
+            count += 1
+
+        if count > 0:
+            answer.append(count)
+
     return answer
